d5d787b1a1e381a4ad12c3706833e5e95fd3c166
freitasjoyce-cmd <freitas.joyce@academico.ifpb.edu.br>
2026-05-20 12:08:37 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,17 @@
 import streamlit as st
+col1, col2, col3 = st.columns(3)
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+with col1:
+    st.image("apple.png", use_container_width=True)
+    st.title("### Apple")
+    st.link_button("Acessar", "https://www.apple.com/br/")
+
+with col2:
+    st.image("netflix.png", use_container_width=True)
+    st.title("### Netflix")
+    st.link_button("Acessar", "https://www.netflix.com/br/")
+
+with col3:
+    st.image("spacex.png", use_container_width=True)
+    st.title("### SpaceX")
+    st.link_button("Acessar", "https://www.spacex.com/")
