613ef26d6c8a8a424bdce44395c628847a632457
freitasjoyce-cmd <freitas.joyce@academico.ifpb.edu.br>
2026-05-20 12:20:28 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,15 +3,17 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.image("apple.jpg", use_container_width=True)
-    st.title("### Apple")
+    st.title("Apple")
     st.link_button("Acessar", "https://www.apple.com/br/")
+    st.write("Empresa responsável por uma das maiores produções de eletrônicos do mundo")
 
 with col2:
     st.image("netflix.jpg", use_container_width=True)
-    st.title("### Netflix")
+    st.title("Netflix")
     st.link_button("Acessar", "https://www.netflix.com/br/")
-
+    st.write("Plataforma de filmes, séries e streaming online.")
 with col3:
     st.image("spacex.jpg", use_container_width=True)
-    st.title("### SpaceX")
+    st.title("SpaceX")
     st.link_button("Acessar", "https://www.spacex.com/")
+    st.write("Empresa espacial que desenvolve foguetes e missões espaciais.")
