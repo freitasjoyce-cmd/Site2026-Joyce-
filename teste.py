23428f416f5b800f772ad1ddcfb0d8eb1a5601f0
freitasjoyce-cmd <freitas.joyce@academico.ifpb.edu.br>
2026-05-20 12:21:57 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import streamlit as st
-col1, col2, col3 = st.columns(3)
 
+st.title("Empresas Parceiras")
+col1, col2, col3 = st.columns(3)
 with col1:
     st.image("apple.jpg", use_container_width=True)
     st.title("Apple")
